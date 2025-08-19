13533b25b89fc3a9aa4c89f816a404eb304e4cfb
Kintu Peter <kintupeter721@gmail.com>
2025-08-19 20:55:19 +0300

    rename

--- user/urls.py
+++ user/urls.py
@@ -8,8 +8,8 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    # URL for the user registration page
-    path('register/', views.register, name='register'),
+    # URL for the user registration page, using the name 'signup' to match the template
+    path('register/', views.register, name='signup'),
     
     # URL for the user login page
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
