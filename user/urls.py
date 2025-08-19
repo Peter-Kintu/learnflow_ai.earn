3a482e23a7c00108d6066d598cdba62bc08b62c2
Kintu Peter <kintupeter721@gmail.com>
2025-08-20 01:32:56 +0300

    rename

--- user/urls.py
+++ user/urls.py
@@ -1,19 +1,21 @@
 # user/urls.py
 
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth import views as auth_views
 
-# Define the application namespace for user-related URLs
-app_name = 'user'
+app_name = "user"
 
 urlpatterns = [
-    # URL for the user registration page, using the name 'signup' to match the template
-    path('register/', views.register, name='signup'),
-    
-    # URL for the user login page
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
-    
-    # URL for the user logout page
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Update this line to use the correct view function name, `register_request`
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('profile/', views.profile_view, name='profile'),
 ]
