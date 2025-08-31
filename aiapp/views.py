e41a675f78081fd96b8420954d2616891c5beca2
Kintu Peter <kintupeter721@gmail.com>
2025-08-31 22:06:56 +0300

    views.PY

--- aiapp/views.py
+++ aiapp/views.py
@@ -1,5 +1,5 @@
-import json
 import io
+import json
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
