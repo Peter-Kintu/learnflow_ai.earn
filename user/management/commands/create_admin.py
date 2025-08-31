fecde1953c277e269631334ed0df115fce7b9a05
Kintu Peter <kintupeter721@gmail.com>
2025-08-31 19:43:48 +0300

    views/aiapp

--- user/management/commands/create_admin.py
+++ user/management/commands/create_admin.py
@@ -1,17 +1,17 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
+# from django.core.management.base import BaseCommand
+# from django.contrib.auth import get_user_model
 
-class Command(BaseCommand):
-    help = 'Create a default superuser'
+# class Command(BaseCommand):
+#     help = 'Create a default superuser'
 
-    def handle(self, *args, **kwargs):
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='LEARNFLOW_AI',
-                email='kintupeter7@gmail.com',
-                password='PETERPETER'
-            )
-            self.stdout.write(self.style.SUCCESS('Superuser created.'))
-        else:
-            self.stdout.write(self.style.WARNING('Superuser already exists.'))
+#     def handle(self, *args, **kwargs):
+#         User = get_user_model()
+#         if not User.objects.filter(username='admin').exists():
+#             User.objects.create_superuser(
+#                 username='',
+#                 email='',
+#                 password=''
+#             )
+#             self.stdout.write(self.style.SUCCESS('Superuser created.'))
+#         else:
+#             self.stdout.write(self.style.WARNING('Superuser already exists.'))
