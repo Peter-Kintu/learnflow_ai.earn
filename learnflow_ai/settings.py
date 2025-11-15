8f77cc141c08e2280e939bbe4be80bbe479fa724
Kintu Peter <kintupeter721@gmail.com>
2025-11-15 20:22:38 +0300

    Updated login page with background slideshow

--- learnflow_ai/settings.py
+++ learnflow_ai/settings.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost,lear
 
 CSRF_TRUSTED_ORIGINS = [
     'https://learnflow-ai-0fdz.onrender.com',
-    'https://learnflow-ai.koyeb.app'
+    'https://learnflow-ai-earn.koyeb.app'
     ]
 
 # Installed Apps
