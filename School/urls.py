dfcd46f85f0cf9a0c9ca8539d35ab575aa828199
Peter Kintu <kintupeter7@gmail.com>
2026-03-21 09:32:23 +0300

    Updated login page with background slideshow

--- School/urls.py
+++ School/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'school'
+
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
     path('fees/', views.fees_list, name='fees_list'),
